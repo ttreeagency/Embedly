ed71b35ac477aef1d2af73f48cead8813afbec00
Dominique Feyer <dfeyer@ttree.ch>
2016-01-14 00:06:35 +0100

    TASK: Initial commit

---
+++ Classes/Embedly.php
@@ -0,0 +1,36 @@
+<?php
+namespace Ttree\CloudButler\EmbedlyAdapter\Action;
+
+/*
+ * This file is part of the Ttree.Embedly package.
+ *
+ * (c) ttree ltd - www.ttree.ch
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * @api
+ */
+class Embedly extends \Embedly\Embedly
+{
+    /**
+     * @Flow\InjectConfiguration(package="Ttree.Embedly")
+     * @var array
+     */
+    protected $settings;
+
+    /**
+     * @param array $args
+     */
+    public function __construct(array $args)
+    {
+        $this->key = $this->settings['apiKey'];
+        parent::__construct($args);
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# Ttree.Embedly
+
+Flow Framework Wrapper around the official library for [Embedly API].
+
+This package is Composer ready, [PSR-2] and [PSR-4] compliant.
+
+Acknowledgments
+---------------
+
+Development sponsored by [ttree ltd - neos solution provider](http://ttree.ch).
+
+We try our best to craft this package with a lots of love, we are open to sponsoring, support request, ... just contact us.
+
+License
+-------
+
+The MIT License (MIT). Please see [LICENSE](LICENSE.txt) for more information.
+
+[PSR-2]: http://www.php-fig.org/psr/psr-2/
+[PSR-4]: http://www.php-fig.org/psr/psr-4/
+[Embedly API]: https://github.com/embedly/embedly-php
